ac0c504050fdb98375cade741ff80f2ee274725f
Hodge-zf <xx.skcs@gmail.com>
2021-03-05 20:01:58 -0600

    Pieceworker class finished

--- assignment1/employee/Pieceworker.java
+++ assignment1/employee/Pieceworker.java
@@ -1,3 +1,33 @@
 public class Pieceworker extends Employee{
 
+  private double piecesProduced;
+  private double employeeWage;
+
+  public Pieceworker(String first, String last, String ssn, double pieces, double wage){
+
+    super(first, last, ssn);
+    setPiecesProduced(pieces);
+    setEmployeeWage(wage);
+  }
+
+  public void setEmployeeWage(double wage){
+    employeeWage = wage;
+  }
+
+  public double getEmployeeWage(){
+    return employeeWage;
+  }
+
+  public void setPiecesProduced(double pieces){
+    piecesProduced = pieces;
+  }
+
+  public double getPiecesProduced(){
+    return piecesProduced;
+  }
+
+  @Override
+  public double earnings(){
+    return getEmployeeWage() * getPiecesProduced();
+  }
 }
