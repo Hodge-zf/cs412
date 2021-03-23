6c4427a5eab993ddc7271f781da7bcc1151bd962
Hodge-zf <xx.skcs@gmail.com>
2021-03-22 22:03:38 -0500

    Done with problem 3

--- assignment3/SummarizeWordFile.java
+++ assignment3/SummarizeWordFile.java
@@ -1,8 +1,6 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.stream.Stream;
-import java.io.PrintStream;
 import java.time.*;
 
 public class SummarizeWordFile{
@@ -10,10 +8,27 @@ public class SummarizeWordFile{
   public static void main(String[] args) throws IOException {
 
       File inputFile = new File("test.txt");
+      File outputFile = new File("output1.txt");
 
       PrintStream stdout = System.out;
-      PrintStream fileStream = new PrintStream("output.txt");
+      PrintStream fileStream = new PrintStream("output2.txt");
       System.setOut(fileStream);
+
+      Instant separateStartSequential = Instant.now();
+
+      FileInputStream inputStream = new FileInputStream(inputFile);
+      FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+      int i;
+
+      while ((i = inputStream.read()) != -1) {
+        outputStream.write(i);
+      }
+
+      inputStream.close();
+      outputStream.close();
+
+      Instant separateEndSequential = Instant.now();
 
       Stream<String> text = Files.lines(inputFile.toPath());
 
@@ -25,6 +40,8 @@ public class SummarizeWordFile{
       Instant separateEndParallel = Instant.now();
 
       System.setOut(stdout);
+
+      System.out.printf("Sequential ran in: %d%n%n",Duration.between(separateStartSequential, separateEndSequential).toMillis());
       System.out.printf("Parallel ran in: %d%n%n",Duration.between(separateStartParallel, separateEndParallel).toMillis());
 
   }
